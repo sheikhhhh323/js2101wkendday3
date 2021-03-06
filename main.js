c2cb164717a6f95aa45a1c86eb80ec627019ddc9
Alabi A <alabi10@yahoo.com>
2021-03-06 17:14:43 +0100

    add index.html and main.js to the repo

---
+++ main.js
@@ -0,0 +1,41 @@
+// let students = ['fred', 'mayowa', 'remi', 'joshua', 'tina', 'arewa', 'seyi'];
+
+// //loop
+// //start
+// //interval
+// //stop
+
+// //while loop
+// let postion = 0;
+// while (postion <= (students.length-1)) {
+//     console.log(students[postion]);
+//     postion = postion + 1;
+// }
+
+// console.log("SEPERATION");
+
+// //do while
+// postion = 0;
+// do{
+//     console.log(students[postion]);
+//     postion = postion + 1;  //== ++postion
+// }
+// while (postion <= (students.length-1));
+
+// console.log("SEPERATION");
+
+// //for
+// for(let index = 0; index < students.length; ++index){
+//      console.log(students[index]);
+// }
+
+
+/* 1 X 2 = 2
+2 X 2 = 4
+3 X 2 = 6
+4 X 2 = 8
+ */
+
+for(let start = 1; start <= 12; ++start){
+    console.log(start + " X 2 = " + (start * 2));
+}
